859a22d155066aafbccca7a8d2fd08665380dd83
habibawahid1112 <habiba.wahid@msa.edu.eg>
2025-05-21 16:22:43 +0300

    Add files via upload

---
+++ RoundedBorder.java
@@ -0,0 +1,52 @@
+package studentmanagementsystem;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author User
+ */
+import com.formdev.flatlaf.FlatLightLaf;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.border.*;
+
+// ---------- RoundedBorder.java ----------
+// Custom Rounded Border class
+class RoundedBorder implements Border {
+    private int radius;
+    private Color borderColor;
+    private int thickness;
+
+    public RoundedBorder(int radius, Color borderColor, int thickness) {
+        this.radius = radius;
+        this.borderColor = borderColor;
+        this.thickness = thickness;
+    }
+
+    @Override
+    public Insets getBorderInsets(Component c) {
+        return new Insets(this.thickness, this.thickness, this.thickness, this.thickness);
+    }
+
+    @Override
+    public boolean isBorderOpaque() {
+        return false;
+    }
+
+    @Override
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(borderColor);
+        g2.setStroke(new BasicStroke(thickness));
+        g2.drawRoundRect(x + thickness/2, y + thickness/2,
+                width - thickness, height - thickness,
+                radius, radius);
+    }
+}
